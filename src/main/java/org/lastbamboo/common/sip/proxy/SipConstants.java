de7533f8fc28686322c0e488a17c4b2f9a89076c
adamfisk <a@littleshoot.org>
2007-11-24 11:42:46 +0000

    Added to comment
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/sip/proxy@1678 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/sip/proxy/SipConstants.java
+++ src/main/java/org/lastbamboo/common/sip/proxy/SipConstants.java
@@ -7,7 +7,9 @@ public class SipConstants
     {
 
     /**
-     * Listening port.
+     * Listening port.  OK, this is crazy, but we don't use 5060 because some
+     * ISPs, particularly in India and Pakistan but likely elsewhere, block
+     * SIP!  Took me awhile to figure that one out from my hotel room in Delhi.
      */
     public static final int SIP_PORT = 5061;
 
